7adb28a5c3b8ffecb63f7444b35ea62016a628fe
Ryan Crowder <crowderyan@gmail.com>
2022-05-15 21:42:46 -0400

    some basic exceptions for search failures

---
+++ GameStore/src/main/java/com/comapny/GameStore/exception/InvalidRequestException.java
@@ -0,0 +1,6 @@
+package com.comapny.GameStore.exception;
+
+public class InvalidRequestException extends RuntimeException{
+    public InvalidRequestException(String message) {super (message);}
+    public InvalidRequestException() {super();}
+}
---
+++ GameStore/src/main/java/com/comapny/GameStore/exception/NoGameFoundException.java
@@ -0,0 +1,5 @@
+package com.comapny.GameStore.exception;
+
+public class NoGameFoundException extends RuntimeException{
+    public NoGameFoundException(String message) { super(message);}
+}
