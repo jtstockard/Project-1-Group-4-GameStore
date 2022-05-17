b8fd616492f2027027fb04437b431fab51de93b4
L II <lii@Ls-MacBook-Pro.lan>
2022-05-17 01:52:34 -0500

    added PUT method in TshirtController

--- GameStore/src/main/java/com/company/GameStore/controller/TshirtsController.java
+++ GameStore/src/main/java/com/company/GameStore/controller/TshirtsController.java
@@ -1,6 +1,7 @@
 package com.company.GameStore.controller;
 
 import com.company.GameStore.models.Consoles;
+import com.company.GameStore.models.Games;
 import com.company.GameStore.models.Tshirts;
 import com.company.GameStore.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,23 +43,28 @@ public class TshirtsController {
         return serviceLayer.findTshirtBySize(size);
     }
 
-    @RequestMapping(value = "/tshirts", method =RequestMethod.PUT)
+    @RequestMapping(value="/tshirts/{color}", method=RequestMethod.PUT)
     @ResponseStatus(HttpStatus.OK)
-    public List<Tshirts> updateTshirts(@RequestBody Tshirts tshirts) {
-        return serviceLayer.updateTshirts(tshirts);
+    public List<Tshirts> updateTshirtsByColor(@PathVariable String color) {
+        return serviceLayer.updateTshirtsByColor();
     }
 
-
-    @RequestMapping(value="/tshirt/{color}", method=RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTshirt(@PathVariable String color) {
-        serviceLayer.removeTshirt(color);
+    @RequestMapping(value="/tshirts/{size}", method=RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Tshirts> updateTshirtsBySize(@PathVariable String size) {
+        return serviceLayer.updateTshirtsBySize();
     }
 
-
-    @RequestMapping(value="/tshirt/{size}", method=RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteTshirt(@PathVariable String size) {
-        serviceLayer.removeTshirt(size);
+    @RequestMapping(value="/games/{studio}", method=RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Games> deleteGamesByStudio(@PathVariable String studio) {
+        return serviceLayer.removeGamesByStudio();
     }
+
+    @RequestMapping(value="/games/{rating}", method=RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Games> deleteGamesByRating(@PathVariable String rating) {
+        return serviceLayer.removeGamesByRating();
+    }
+
 }
