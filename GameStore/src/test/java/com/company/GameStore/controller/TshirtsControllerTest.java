9a5f93f581787d2d4b74ace330d1b0dfc88f95e6
L II <lii@Ls-MacBook-Pro.lan>
2022-05-17 03:09:32 -0500

    updated test in TshirtsControllerTest

--- GameStore/src/test/java/com/company/GameStore/controller/TshirtsControllerTest.java
+++ GameStore/src/test/java/com/company/GameStore/controller/TshirtsControllerTest.java
@@ -1,10 +1,44 @@
 package com.company.GameStore.controller;
 
+import com.company.GameStore.models.Consoles;
+import com.company.GameStore.models.Tshirts;
+import com.company.GameStore.service.ServiceLayer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(TshirtsController.class)
+
 public class TshirtsControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ServiceLayer serviceLayer;
+
+    private Tshirts tshirts;
+
+    private String tshirtsJson;
+
+    private List<Tshirts> allTshirts= new ArrayList<>();
+
+    private String allTshirtssJson;
+
+
+    private ObjectMapper mapper = new ObjectMapper();
 
     @Test
     public void getAllTshirts() {
